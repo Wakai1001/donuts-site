478a67f31c90968bbda21482dc61e5c9f3548c7a
kanata-1115 <cca.ta1115.cca@gmail.com>
2025-03-28 11:54:51 +0900

    画像追加しました

---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../common/css/reset.css">
+<link rel="stylesheet" href="../common/css/common.css">
+</head>
+<body>
+  
+<p><a href="">よくある質問</a></p>
+<p><a href="">お問い合わせ</a></p>
+<p><a href="">当サイトのポリシー</a></p>
+
+<a href="#"><img src="../images/" alt="インスタグラムのアイコン"></a>
+<a href="#"><img src="../images/" alt="フェイスブックのアイコン"></a>
+<a href="#"><img src="../images/" alt="エックスのアイコン"></a>
+
+</body>
+</html>
