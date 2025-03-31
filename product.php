87aaa37071926c1b7fb15129844e067f712d7c03
piyoko7 <ysank13gr@gmail.com>
2025-03-31 15:35:02 +0900

    商品一覧更新

--- product.php
+++ product.php
@@ -1,5 +1,28 @@
 <?php require 'includes/header.php' ?>
+<!-- 商品一覧　松井 -->
 
+<main>
+   <!-- パンくずナビゲーション -->
+<nav aria-label="page">
+        <ul class="catalog_page">
+          <li><a href="../index.php">TOP</a></li>
+          <li aria-current="page">商品一覧</li>
+        </ul>
+      </nav>
+      <!-- ここまでパンくずナビゲーション -->
+
+      ようこそ ゲスト様
+
+      商品一覧
+
+<img src="" alt="">
+
+</main>
+
+
+     
+     
+      
 
 
 
