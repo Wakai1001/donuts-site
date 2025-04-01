ea6b5b0a9def963459cdc624ed49838c772576fe
mkpq <mmmpq.pro@gmail.com>
2025-04-01 12:55:49 +0900

    confirm 途中

--- customer-confirm.php
+++ customer-confirm.php
@@ -1,6 +1,16 @@
-<?php require 'includes/header.php' ?>
+<?php require 'includes/header.php'; ?>
+<?php require 'includes/databese.php'; ?>
 
+<?php
 
+echo '<h2>ご入力内容の確認</h2>'; 
+echo '<div class=name>';
+echo '<p class=title>お名前</p>';
+echo '<p class=confirm>"',$name,'"</p></div>';
+echo '<div class=kana>';
+echo '<p class=title>お名前（フリガナ）</p>';
+echo '<p class=confirm>"',$kana,'"</p></div>';
 
+?>
 
-<?php require 'includes/footer.php' ?>
+<?php require 'includes/footer.php'; ?>
