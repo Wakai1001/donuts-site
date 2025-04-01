c60ac56ba00452f90ba558257ed31fd3946cdca1
piyoko7 <ysank13gr@gmail.com>
2025-04-01 15:52:02 +0900

    商品一覧更新

---
+++ product.php
@@ -0,0 +1,88 @@
+<?php require 'includes/header.php' ?>
+
+<?php
+
+// <!-- 商品一覧　松井 -->
+
+echo '<main>';
+  // <!-- パンくずリスト -->
+  echo '<nav aria-label="page">';
+    echo '<ul class="catalog_page">';
+      echo '<li><a href="../index.php">TOP</a></li>';
+      echo '<li aria-current="page">商品一覧</li>';
+      echo '</ul>';
+      echo '</nav>';
+  // <!-- ここまでパンくずリスト -->
+
+  // <!-- ユーザー名 -->
+  echo '<p>ようこそ ゲスト様</p>';
+
+  // <!--     商品一覧     -->
+  echo '<h2>商品一覧</h2>';
+
+  echo '<img src="common/images/cc-donut.jpg" alt="">';
+  echo '<p>CCドーナツ 当店オリジナル（5個入り）</p>';
+  echo '<p>税込 ¥1,500 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/chocolate-delight.jpg" alt="">';
+  echo '<p>チョコレートデライト（5個入り）</p>';
+  echo '<p>税込 ¥1,600 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/caramel-cream.jpg" alt="">';
+  echo '<p>キャラメルクリーム（5個入り）</p>';
+  echo '<p>税込 ¥1,600 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/plain-classic.jpg" alt="">';
+  echo '<p>プレーンクラシック（5個入り）</p>';
+  echo '<p>税込 ¥1,500 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/summer-citrus.jpg" alt="">';
+  echo '<p>【新作】サマーシトラス（5個入り）</p>';
+  echo '<p>税込 ¥1,600 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/strawberry-crush.jpg" alt="">';
+  echo '<p>ストロベリークラッシュ（5個入り）</p>';
+  echo '<p>税込 ¥1,800 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  // <!--     バラエティセット     -->
+  echo '<h3>バラエティセット</h3>';
+
+  echo '<img src="common/images/fruit-donuts-1.jpg" alt="">';
+  echo '<p>フルーツドーナツセット（12個入り）</p>';
+  echo '<p>税込 ¥3,500 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/fruit-donuts-2.jpg" alt="">';
+  echo '<p>フルーツドーナツセット（14個入り）</p>';
+  echo '<p>税込 ¥4,000 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/best-selection.jpg" alt="">';
+  echo '<p>ベストセレクションボックス（4個入り）</p>';
+  echo '<p>税込 ¥1,200 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/chocolate-crush-box.jpg" alt="">';
+  echo '<p>チョコクラッシュボックス（7個入り）</p>';
+  echo '<p>税込 ¥2,400 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/cream-box-1.jpg" alt="">';
+  echo '<p>クリームボックス（4個入り）</p>';
+  echo '<p>税込 ¥1,400 ♡</p>';
+  echo '<p>カートに入れる</p>';
+
+  echo '<img src="common/images/cream-box-2.jpg" alt="">';
+  echo '<p>クリームボックス（9個入り）</p>';
+  echo '<p>税込 ¥2,800 ♡</p>';
+  echo '<p>カートに入れる</p>';
+  echo '</main>';
+
+?>
+<?php require 'includes/footer.php' ?>
