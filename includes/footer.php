a3ee78f37a2aaab782d7b9f15f2986e0095b7af7
kanata-1115 <cca.ta1115.cca@gmail.com>
2025-04-09 11:47:52 +0900

    ドロワーメニューとfooterダミーいれました

--- includes/footer.php
+++ includes/footer.php
@@ -2,9 +2,9 @@
 <footer>
   <div class="footer_design">
     <div id="footer_nav">
-      <p><a href="">よくある質問</a></p>
-      <p><a href="">お問い合わせ</a></p>
-      <p><a href="">当サイトのポリシー</a></p>
+      <p><a href="#">よくある質問</a></p>
+      <p><a href="#">お問い合わせ</a></p>
+      <p><a href="#">当サイトのポリシー</a></p>
     </div>
     
     <div id="sns_area">
