910ba86e0abb37b0ae53b9a5b20279cfa0a96794
yuhirata3018 <rocksteady3018@gmail.com>
2025-04-09 15:56:36 +0900

    test復活

--- index.php
+++ index.php
@@ -3,7 +3,81 @@
   require 'includes/database.php';
   require 'includes/header.php';
   ?>
+<?php
+$donuts1 =[
+  'id' => 1,
+  'name' => 'CCドーナツ 当店オリジナル (5個入り)',
+  'price' => 1500
+];
 
+$donuts2 =[
+  'id' => 2,
+  'name' => 'チョコレートデライト（5個入り）',
+  'price' => 1600
+];
+
+$donuts3 =[
+  'id' => 3,
+  'name' => 'キャラメルクリーム（5個入り）',
+  'price' => 1600
+];
+
+$donuts4 =[
+  'id' => 4,
+  'name' => 'プレーンクラシック（5個入り）',
+  'price' => 1500
+];
+
+$donuts5 =[
+  'id' => 5,
+  'name' => '【新作】サマーシトラス（5個入り）',
+  'price' => 1600
+];
+
+$donuts6 =[
+  'id' => 6,
+  'name' => 'ストロベリークラッシュ（5個入り）',
+  'price' => 1800
+];
+
+$donuts7 =[
+  'id' => 7,
+  'name' => 'フルーツドーナツセット（12個入り）',
+  'price' => 3500
+];
+
+$donuts8 =[
+  'id' => 8,
+  'name' => 'フルーツドーナツセット（14個入り）',
+  'price' => 4000
+];
+
+$donuts9 =[
+  'id' => 9,
+  'name' => 'ベストセレクションボックス（4個入り）',
+  'price' => 1200
+];
+
+$donuts10 =[
+  'id' => 10,
+  'name' => 'チョコクラッシュボックス（7個入り）',
+  'price' => 2400
+];
+
+$donuts11 =[
+  'id' => 11,
+  'name' => 'クリームボックス（4個入り））',
+  'price' => 1400
+];
+
+$donuts12 =[
+  'id' => 12,
+  'name' => 'クリームボックス（9個入り）',
+  'price' => 2800
+];
+
+
+?>
 <main>
 
 <?php if(isset($_SESSION['customer'])): ?>
@@ -15,9 +89,10 @@
   <img src="common/images/hero-sp.jpg" alt="main visual" class="hero_sp">
   <div class="content1">
     <div class="forseal_container">
-      <img src=" common/images/summer-citrus.jpg" class="image-content1" alt="サマーシトラスドーナツ">
+    <a href="detail.php?id=5">
+      <img src=" common/images/donuts5.jpg" class="image-content1" alt="サマーシトラスドーナツ">
       <img src="common/images/new-products-icon.png" alt="新商品のシール" class="new-products-icon">
-      <p class="content_text">サマーシトラス</p>
+      <p class="content_text">サマーシトラス</p></a>
     </div>
     <div class="forlife_container">
       <img src="common/images/donut-life.jpg" class="image-content2" alt="ドーナツのある生活">
